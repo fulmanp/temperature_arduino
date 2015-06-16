55a2aa6200effbddb968ff432a344d0df749fb77
Piotr Fulmański <piotr@fulmanski.pl>
2015-06-16 15:24:15 +0200

    Stable version after code reorganization, no code for BT

--- ds18b20_multi/ds18b20_multi.ino
+++ ds18b20_multi/ds18b20_multi.ino
@@ -1,88 +1,97 @@
 #include <OneWire.h>
 #include <LiquidCrystal.h>
-#include <dht.h>
+#include "dht.h"
 
-#define BUTTON_PIN 3
-#define DATA_LED_PIN  4
-#define DHT_PIN 5
+#define PIN_BUTTON 3
+#define PIN_DATA_LED  4
+#define PIN_DHT_1 5
+#define INTERVAL_TEMP_READ 1000*60
+#define SCREEN_DS18B20 0
+#define SCREEN_DHT 1
+#define MAX_DS18B20 4
+#define DISP_WIDTH 20
 
-dht DHT;
-// OneWire DS18S20, DS18B20, DS1822 Temperature Example
-//
-// http://www.pjrc.com/teensy/td_libs_OneWire.html
-//
-// The DallasTemperature library can do all this work for you!
-// http://milesburton.com/Dallas_Temperature_Control_Library
+dht DHT_1;
 
-OneWire  ds(2);  // on pin 10 (a 4.7K resistor is necessary)
+OneWire  ds(2);                         // on pin 2 (a 4.7K resistor is necessary)
 LiquidCrystal lcd(12, 11, 10, 9, 8, 7); //lcd(5, 6, 9, 10, 11, 12);
 
-int counter = 0;
-bool flag = false;
-byte i;
-byte present = 0;
-byte type_s;
-byte data[12];
-byte addr[8];
-float celsius, fahrenheit;
-String result = "";
-String dhtResult = "";
-char val;
+typedef struct 
+{
+  float celsius;
+  float fahrenheit;
+  String rom;    // ROM as readable string
+  byte addr[8];  // ROM as array of bytes
+  bool valid;
+} ds18b20Sensor;
+
+int counter = 0; // for DS18B20; iterator which counts the number of sensors for current temp read
+ds18b20Sensor ds18b20Sensors[MAX_DS18B20];
+
+int screen = SCREEN_DS18B20; // indicates what screen should be printed: for DS18B20 or DHT sensor(s)
+byte i, j;        // for loop iterator
+byte present = 0; // for DS18B20 - taken form example, not sure if we need this
+byte type_s;      // for DS18B20 ???
+byte data[12];    // for DS18B20
+byte *addr;       // for DS18B20
+String tmpString = "";  // variable used to prepare string to print on lcd
+char valFromSerial;     // variable used to keep value send by serial (from BT)
+
+unsigned long lastMillisTemperatureRead = 0;
+unsigned long tick = 0; // Counts how many times temperature was read
+unsigned long d, h, m;  // variables used to print "time"
+
+// Local variable used by timeElapsed() function
+unsigned long currentMillis;
+unsigned long diff;
 
 void setup(void) {
-  //Serial.begin(9600);// default for terminal
-  Serial.begin(19200);// for my bt module
-  pinMode(DATA_LED_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT);
-  attachInterrupt(1, button, CHANGE);
-  // Choose screen size - 20x4
-  lcd.begin(20, 4); //lcd.begin(16, 2); 
-  lcd.clear();
-  // Display
-  lcd.print("Temp & Humid");
-  delay(1000);
-  lcd.clear();
+  Serial.begin(19200); // 9600 is default for terminal. For my BT module use 19200
+  pinMode(PIN_DATA_LED, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT);
+  attachInterrupt(1, switchScreen, CHANGE);  
+  initDisplay();
+  readTemp();
+  printTemp();
 }
 
 void loop(void) {
-//  int state = digitalRead(BUTTON_PIN);
-//  
-//  if(state == HIGH){
-//    flag = !flag;  // button pressed
-//    //digitalWrite(DATA_LED_PIN, HIGH);
-//  }
-  
-  //int flag;
-  //delay(1000);
-  //lcd.clear();
-  if(Serial.available() <= 0){
-    if(flag == false){  // Read from DHT21
-      readDHT();
-    }
-    else if(flag == true){
-      //lcd.clear();
-      read();
-    }
-  }
-
-  //lcd.clear();
-  if(Serial.available() > 0){    
-   lcd.clear(); 
-    val = Serial.read();
-    //Serial.println(">");   
-    //Serial.println(val);
-    //Serial.println("<");
-    //flag=0;
-    
-    if (val == '1' || val == '0'){
-      if (val == '1'){
-        Serial.println("1 received");
-        digitalWrite(DATA_LED_PIN, HIGH);
-        delay(100);
-        digitalWrite(DATA_LED_PIN, LOW);
-      }
-      
-      read();
+  if(timeElapsed(&lastMillisTemperatureRead, INTERVAL_TEMP_READ)){
+    tick++;
+    readTemp();
+    printTemp();
+  } else {
+    if(Serial.available() > 0){    
+      valFromSerial = Serial.read();  
+      if (valFromSerial == '1' || valFromSerial == '0'){
+        if (valFromSerial == '1'){
+          Serial.println("1 received");
+          digitalWrite(PIN_DATA_LED, HIGH);
+          delay(100);
+          digitalWrite(PIN_DATA_LED, LOW);
+        }      
+        readTemp();
+        printTemp();
+        //sendTemp(); // Not yet implemented
+     }
    }
- }
+  }  
+}
+
+void initDisplay(){
+  lcd.begin(20, 4); // Choose screen size - 20x4
+  lcd.clear();
+  lcd.setCursor(0,0);  
+  lcd.print("      Welcome!      ");
+  lcd.setCursor(0,1);
+  lcd.print("    Temp & Humid    ");
+  lcd.setCursor(0,2);
+  lcd.print("      Station       ");
+  delay(2000);
+  lcd.clear();
+}
+
+void readTemp(){
+  readTempDS18B20();
+  readTempDHT();
 }
--- ds18b20_multi/functions.ino
+++ ds18b20_multi/functions.ino
@@ -1,163 +1,183 @@
-//byte i;
-//byte present = 0;
-//byte type_s;
-//byte data[12];
-//byte addr[8];
-//float celsius, fahrenheit;
-//String result = "";
-//String dhtResult = "";
-//char val;
+void readTempDS18B20(void){   
+  counter = 0;  
+  while(1){ // Main loop START
+    addr = ds18b20Sensors[counter].addr;
+    if ( !ds.search(addr)) { // No more addresses.
+      ds.reset_search();
+      delay(250);
+      break;
+    }
+      
+    ds18b20Sensors[counter].valid = true;    
+    ds18b20Sensors[counter].rom = "";
+      
+    if (OneWire::crc8(addr, 7) != addr[7]) {
+      //Serial.println("CRC is not valid!");
+      ds18b20Sensors[counter].valid = false;
+      return;
+    }
 
-void read(){  
-  result += "BEGIN";
-        
-  while(1){
-  if ( !ds.search(addr)) {
-          //Serial.println("No more addresses.");
-          //Serial.println();
-    ds.reset_search();
-    delay(250);
-    break;
-  }
+    // the first ROM byte indicates which chip
+    switch (addr[0]) {
+      case 0x10:
+        //Serial.println("  Chip = DS18S20");  // or old DS1820
+        type_s = 1;
+        break;
+      case 0x28:
+        //Serial.println("  Chip = DS18B20");
+        type_s = 0;
+        break;
+      case 0x22:
+        //Serial.println("  Chip = DS1822");
+        type_s = 0;
+        break;
+      default:
+        //Serial.println("Device is not a DS18x20 family device.");
+        return;
+    } // switch
       
-        //Serial.print("ROM =");
-        result += "ROM";
-        for( i = 0; i < 8; i++) {
-          //Serial.write(' ');
-          //Serial.print(addr[i], HEX);
-          
-          if (addr[i]<16)
-            result += "0";
-          result += String(addr[i], HEX);
-        }
-      
-        if (OneWire::crc8(addr, 7) != addr[7]) {
-            //Serial.println("CRC is not valid!");
-            result += 'ERROR';
-            return;
-        }
-        //Serial.println();
-      
-        // the first ROM byte indicates which chip
-        switch (addr[0]) {
-          case 0x10:
-            //Serial.println("  Chip = DS18S20");  // or old DS1820
-            type_s = 1;
-            break;
-          case 0x28:
-            //Serial.println("  Chip = DS18B20");
-            type_s = 0;
-            break;
-          case 0x22:
-            //Serial.println("  Chip = DS1822");
-            type_s = 0;
-            break;
-          default:
-            //Serial.println("Device is not a DS18x20 family device.");
-            return;
-        } 
-      
-        ds.reset();
-        ds.select(addr);
-        ds.write(0x44);        // start conversion, use ds.write(0x44,1) with parasite power on at the end
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44);  // start conversion, use ds.write(0x44,1) with parasite power on at the end
     
-        delay(2000);     // maybe 750ms is enough, maybe not
-        // we might do a ds.depower() here, but the reset will take care of it.
+    delay(2000);     // maybe 750ms is enough, maybe not
+    // we might do a ds.depower() here, but the reset will take care of it.
       
-        present = ds.reset();
-        ds.select(addr);    
-        ds.write(0xBE);         // Read Scratchpad
+    present = ds.reset();
+    ds.select(addr);    
+    ds.write(0xBE);  // Read Scratchpad
       
-        //Serial.print("  Data = ");
-        //Serial.print(present, HEX);
-        //Serial.print(" ");
-        for ( i = 0; i < 9; i++) {           // we need 9 bytes
-          data[i] = ds.read();
-          //Serial.print(data[i], HEX);
-          //Serial.print(" ");
-        }
-        //Serial.print(" CRC=");
-        //Serial.print(OneWire::crc8(data, 8), HEX);
-        //Serial.println();
+    //Serial.print("  Data = ");
+    //Serial.print(present, HEX);
+    for ( i = 0; i < 9; i++) {           // we need 9 bytes
+      data[i] = ds.read();
+    }
       
-        // Convert the data to actual temperature
-        // because the result is a 16 bit signed integer, it should
-        // be stored to an "int16_t" type, which is always 16 bits
-        // even when compiled on a 32 bit processor.
-        int16_t raw = (data[1] << 8) | data[0];
-        if (type_s) {
-          raw = raw << 3; // 9 bit resolution default
-          if (data[7] == 0x10) {
-            // "count remain" gives full 12 bit resolution
-            raw = (raw & 0xFFF0) + 12 - data[6];
-          }
-        } else {
-          byte cfg = (data[4] & 0x60);
-          // at lower res, the low bits are undefined, so let's zero them
-          if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-          else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-          else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-          //// default is 12 bit resolution, 750 ms conversion time
-        }
-        celsius = (float)raw / 16.0;
-        fahrenheit = celsius * 1.8 + 32.0;
-        //Serial.print("  Temperature = ");
-        //Serial.print(celsius);
-        //Serial.print(" Celsius, ");
-        //Serial.print(fahrenheit);
-        //Serial.println(" Fahrenheit");
-        result += celsius;
-        
-        //lcd.clear();
-        display();
+    // Convert the data to actual temperature
+    // because the result is a 16 bit signed integer, it should
+    // be stored to an "int16_t" type, which is always 16 bits
+    // even when compiled on a 32 bit processor.
+    int16_t raw = (data[1] << 8) | data[0];
+    if (type_s) {
+      raw = raw << 3; // 9 bit resolution default
+      if (data[7] == 0x10) {
+        // "count remain" gives full 12 bit resolution
+        raw = (raw & 0xFFF0) + 12 - data[6];
       }
-      
-      if(counter >= 2)
-        counter = 0;
-      
-      result += "END#";    
-            
-      //if(flag == 0){
-        Serial.println(result);
-        //flag = 1;
-      //}
-      
-      delay(2000);
-      //lcd.clear();
-      result = "";
-}
-
-void display(void){
-  lcd.setCursor(0, 0);
-  lcd.print("DS18b20");
-  lcd.setCursor(0, counter % 3 + 1);
-  lcd.print(celsius);
-  counter += 1;
-}
-
-void readDHT(){
-  //lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("DHT 21");
-      lcd.setCursor(0, 1);
-      DHT.read22(DHT_PIN);
-      dhtResult += "Temp: ";
-      dhtResult += DHT.temperature;
-      lcd.print(dhtResult);
-      dhtResult = "";
-      //delay(1000);
-      lcd.setCursor(0, 2);
-      dhtResult += "Humidity: ";
-      dhtResult += DHT.humidity;
-      lcd.print(dhtResult);
-      delay(5000);
-      dhtResult = "";
-}
-
-void button(){
-  if(digitalRead(BUTTON_PIN) == HIGH){
-    flag = !flag;  // button pressed
-    lcd.clear();
-    //digitalWrite(DATA_LED_PIN, HIGH);
+    } else {
+      byte cfg = (data[4] & 0x60);
+      // at lower res, the low bits are undefined, so let's zero them
+      if (cfg == 0x00) raw = raw & ~7;      // 9 bit resolution, 93.75 ms
+      else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+      else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+      //// default is 12 bit resolution, 750 ms conversion time
+    }
+    ds18b20Sensors[counter].celsius = (float)raw / 16.0;
+    ds18b20Sensors[counter].fahrenheit = ds18b20Sensors[counter].celsius * 1.8 + 32.0;
+    counter++;
+    if(counter >= MAX_DS18B20){
+      break;
+    }
+  }// Main loop END
+  
+  for(; counter<MAX_DS18B20; ++counter){
+    ds18b20Sensors[counter].valid = false;    
   }
+}
+
+void printTemp(void){
+  lcd.clear();
+  if (screen == SCREEN_DS18B20){
+    for(i=0; i<MAX_DS18B20; ++i){
+      if(ds18b20Sensors[i].valid){
+        lcd.setCursor(0, i);
+        tmpString = "";                
+        tmpString += ds18b20Sensors[i].celsius;
+        tmpString += " ";
+        ds18b20Sensors[i].rom = "";
+        for(j=0; j<4; ++j){
+          if (ds18b20Sensors[i].addr[j]<16)
+            ds18b20Sensors[i].rom += "0";
+          ds18b20Sensors[i].rom += String(ds18b20Sensors[i].addr[j], HEX);
+        }
+        tmpString += ds18b20Sensors[i].rom;
+        lcd.print(tmpString);
+      }
+    } 
+  } else if (screen == SCREEN_DHT){
+    tmpString = "T:";
+    tmpString += DHT_1.temperature;
+    tmpString += " H:";
+    tmpString += DHT_1.humidity;    
+    lcd.setCursor(0,0);
+    lcd.print(tmpString);
+  }
+  
+  // print "time"
+  lcd.setCursor(0,2);
+  tmpString = "Ticks:";
+  tmpString += tick;
+  lcd.print(tmpString);
+  lcd.setCursor(0,3);
+  h = tick/60;
+  m = tick - h*60;
+  d = h/24;
+  h = h - d*24;
+  // For tick testing
+  // 1 hour = 6 minutes
+  // 1 day = 2 hours
+//  h = tick/6;
+//  m = tick - h*6;
+//  d = h/2;
+//  h = h - d*2;
+  tmpString = "D:";
+  tmpString += d;
+  tmpString += " H:";
+  tmpString += h;
+  tmpString += " M:";
+  tmpString += m;
+  lcd.print(tmpString);
+   
+      
+}
+
+void sendTemp(){
+//  result += "BEGIN";
+//  result += "ROM";
+//  result += "END#";
+}
+
+void readTempDHT(void){
+  DHT_1.read22(PIN_DHT_1);  
+}
+
+void switchScreen(void){
+  if(digitalRead(PIN_BUTTON) == HIGH){
+    screen = (screen == SCREEN_DS18B20 ? SCREEN_DHT : SCREEN_DS18B20);
+    printTemp();
+  }
+}
+
+boolean timeElapsed(unsigned long *previousMillis, unsigned int interval) 
+{
+ currentMillis = millis();
+ diff = currentMillis - *previousMillis;
+ 
+ if(diff < 0){
+   // millis() returns the number of milliseconds since the Arduino board
+   // began running the current program. This number will overflow (go back to zero),
+   // after approximately 50 days.
+   // After overflow, diff will be negative so we have to set up previousMillis.
+   *previousMillis = currentMillis;
+ }
+ else {
+   if(diff >= interval)
+   {
+     *previousMillis = currentMillis;
+     return true;
+   }
+   else {
+     return false;
+   }
+ }
 }
