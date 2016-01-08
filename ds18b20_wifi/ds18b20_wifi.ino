8654fde296bb7e5e6d01691ed6e72c0c12134ea1
Piotr Fulmański <piotr@fulmanski.pl>
2016-01-09 00:33:32 +0100

    Improved basic version.
    - All AT commands are now in separate functions.
    - There is a more or less general 'sendMesage' function.

--- ds18b20_wifi/ds18b20_wifi.ino
+++ ds18b20_wifi/ds18b20_wifi.ino
@@ -1,9 +1,15 @@
 #define AT_COMMAND_DELAY 2000
 #define WIFI_AP_CONNECT 20000
 #define WIFI_TCP_CONNECT 5000
+#define MAX_SEND_TRIALS 3
 
 void setup(void) {
-  int maxTrialsAT = 10;
+  
+  char *ssid = "FunBox-FC08";
+  char *password = "61EA91C5CA152A6E1793ADA434";
+  char *server = "fulmanski.pl";
+  char *method = "GET";
+  char *uri = "/php/temperature/test.php?a=1";
   
   delay(3000);
   
@@ -11,27 +17,8 @@ void setup(void) {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  
-  while(!sendATCommand("AT", "OK", AT_COMMAND_DELAY) && maxTrialsAT){
-    maxTrialsAT--;
-    delay(1000);    
-  }
-  
-  sendATCommand("AT+CWMODE=1", "OK", AT_COMMAND_DELAY);
-  sendATCommand("AT+CWJAP=\"FunBox-FC08\",\"61EA91C5CA152A6E1793ADA434\"", "OK", WIFI_AP_CONNECT);
-  sendATCommand("AT+CIPSTART=\"TCP\",\"fulmanski.pl\",80", "OK", WIFI_TCP_CONNECT);
-  sendATCommand("AT+CIPSEND=66", "OK", AT_COMMAND_DELAY);
-  Serial.write("GET /php/temperature/test.php?a=1 HTTP/1.1");
-  Serial.write(13);
-  Serial.write(10);
-  Serial.write("Host: fulmanski.pl");
-  Serial.write(13);
-  Serial.write(10);
-  Serial.write("");
-  Serial.write(13);
-  Serial.write(10);
-  sendATCommand("AT+CIPCLOSE", "OK", AT_COMMAND_DELAY);
-  sendATCommand("AT+CWQAP", "OK", AT_COMMAND_DELAY);         
+
+  sendMessage(ssid, password, server, method, uri, MAX_SEND_TRIALS);          
 }
 
 void loop(void) {
--- ds18b20_wifi/functions.ino
+++ ds18b20_wifi/functions.ino
@@ -20,3 +20,109 @@ boolean sendATCommand(String ATCommand, String replay, int timeToWaitForComplete
   
   return false;
 }
+
+boolean commandAT_AT(){
+  return sendATCommand("AT", "OK", AT_COMMAND_DELAY);
+}
+
+boolean commandAT_CWMODE(){
+  return sendATCommand("AT+CWMODE=1", "OK", AT_COMMAND_DELAY);
+}
+
+boolean commandAT_CWJAP(char* ssid, char* password){
+  char command[256];
+  sprintf(command, "AT+CWJAP=\"%s\",\"%s\"", ssid, password);
+  return sendATCommand(command, "OK", WIFI_AP_CONNECT);
+}
+
+boolean commandAT_CIPSTART(char* server){
+  char command[256];
+  sprintf(command, "AT+CIPSTART=\"TCP\",\"%s\",80", server);
+  return sendATCommand(command, "OK", WIFI_TCP_CONNECT);
+}
+
+boolean commandAT_CIPSEND(char* method, char* uri, char* server){
+  int len = 6;
+  char command[256];
+  char requestStartLine[256];   
+  char requestHeaderHost[256];
+  boolean result;
+  
+  sprintf(requestStartLine, "%s %s HTTP/1.1", method, uri);
+  len += strlen(requestStartLine);
+  sprintf(requestHeaderHost, "Host: %s", server);
+  len += strlen(requestHeaderHost);
+  sprintf(command, "AT+CIPSEND=%d", len);
+  if(result=sendATCommand(command, "OK", AT_COMMAND_DELAY)){  
+    Serial.write(requestStartLine);
+    Serial.write(13);
+    Serial.write(10);
+    Serial.write(requestHeaderHost);
+    Serial.write(13);
+    Serial.write(10);
+    Serial.write("");
+    Serial.write(13);
+    Serial.write(10);
+  }  
+  return result; 
+}
+
+boolean commandAT_CIPCLOSE(){
+  return sendATCommand("AT+CIPCLOSE", "OK", AT_COMMAND_DELAY);
+}
+
+boolean commandAT_CWQAP(){
+  return sendATCommand("AT+CWQAP", "OK", AT_COMMAND_DELAY);
+}
+
+
+boolean sendMessage(char*  ssid, char*  password, char* server, char* method, char* uri, int maxTrials){
+  while(_sendMessage(ssid, password, server, method, uri) && maxTrials){
+    maxTrials--;
+  }
+  if(maxTrials)
+    return true;
+  return false;
+}
+
+int _sendMessage(char*  ssid, char*  password, char* server, char* method, char* uri){
+  int maxTrialsAT = 5;
+  
+  while(!commandAT_AT() && maxTrialsAT){
+    delay(1000);
+    maxTrialsAT--;
+  }
+  if(maxTrialsAT){
+    if(commandAT_CWMODE()){
+      if(commandAT_CWJAP(ssid, password)){       
+        if(commandAT_CIPSTART(server)){          
+          if(commandAT_CIPSEND(method, uri, server)){                
+            if(!commandAT_CIPCLOSE())
+              return 6;
+            if(!commandAT_CWQAP())
+              return 7;
+          }// CIPSEND
+          else {
+            return 5;
+          } 
+        }// CIPSTART
+        else {
+          return 4;
+        } 
+      }// CWJAP
+      else {
+        return 3;
+      } 
+    }// CWMODE
+    else {
+      return 2;
+    }    
+  }// maxTrials
+  else {
+    return 1;
+  }
+  
+  return 0;
+}
+
+
